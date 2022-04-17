c60730f9055fdd1c682c1d63836a20949d19c83a
Shem Antipas <50667277+Shem-Antipas@users.noreply.github.com>
2022-04-17 05:00:25 +0300

    CRUD OPERATIONS IN SPRINGBOOT

--- corseAPI/src/main/java/io/shem/springboot/topics/TopicsController.java
+++ corseAPI/src/main/java/io/shem/springboot/topics/TopicsController.java
@@ -31,5 +31,15 @@ public List<Topic> getAllTopics () {
 		topicService.addTopic(topic);
 		
 	}
+	@RequestMapping (method=RequestMethod.PUT, value="/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id){
+		topicService.updateTopic(id, topic);
+		
+	}
+	@RequestMapping (method=RequestMethod.DELETE, value="/topics/{id}")
+	public void deleteTopic(@PathVariable String id){
+		topicService.deleteTopic(id);
+		
+	}
 	}
 
